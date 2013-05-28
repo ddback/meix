bfb655453e76854e8ca4b38575dc5c3ab6406e31
huwence <huwence@gmail.com>
2013-05-28 17:56:34 +0800

    Initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meix::Application.config.secret_key_base = '16209f2decced5132dcc4347aea58437aaf2f25d9b7b7509b90bfbf09c3d122f27a98e17a45c2f5dd45b95c7f332cca41cde64386ae4c548d9f0f81d0846c0b7'
